75e44a2fa223c92699a2a19233a0c8bf23984f65
Yeliz98 <yelizoe98@hotmail.de>
2019-12-01 19:36:06 +0100

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { GroupComponent } from './group/group.component';
+import { TeamListComponent } from './team/team-list/team-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, GroupComponent, TeamListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/team/model/team.ts
@@ -0,0 +1,9 @@
+export class Team {
+
+  constructor(public imageUrl: string,
+              public name: string,
+              public position: string,
+              public room: number,
+              public phone: number,
+              public emal: string) {}
+}
