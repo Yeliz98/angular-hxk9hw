2d68916682ac3c16fcdfeb3025bfc368ef7d4ad6
Yeliz98 <yelizoe98@hotmail.de>
2019-12-01 19:41:53 +0100

    2 files modified

--- src/app/team/team-list/team-list.component.ts
+++ src/app/team/team-list/team-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Team } from '../model/team';
 
 @Component({
   selector: 'app-team-list',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamListComponent implements OnInit {
 
+  public team: Array<Team>;
+
   constructor() { }
 
   ngOnInit() {
+
+     this.team = [
+      new Team('https://hs-flensburg.de/sites/default/files/styles/portraitfoto/public/2017-11/petersen_kai.jpg.jpeg?itok=JVf1P7aR', 
+      'Kai Petersen',
+       'Professor',
+        206,
+        8051470,
+        'kai.petersen@hs-flensburg.de'),
+      new Team('https://hs-flensburg.de/sites/default/files/styles/portraitfoto/public/2017-11/petersen_kai.jpg.jpeg?itok=JVf1P7aR', 
+      'Sönke Cordts',
+       'Professor',
+        206,
+        8051470,
+        'kai.petersen@hs-flensburg.de'),
+      new Team('https://hs-flensburg.de/sites/default/files/styles/portraitfoto/public/2017-11/petersen_kai.jpg.jpeg?itok=JVf1P7aR', 
+      'Jan Gerken',
+       'Professor',
+        206,
+        8051470,
+        'kai.petersen@hs-flensburg.de')
+    ];
   }
 
 }
