d585427925b6c8f39951568687e564dc2399463a
Yeliz98 <yelizoe98@hotmail.de>
2019-12-02 11:04:26 +0100

    5 files modified

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from '../model/event';
 
 @Component({
   selector: 'app-event-list',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventListComponent implements OnInit {
 
+  public event: Array<Event>;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.event = [
+      new Event(
+        'Cloud-Architekturen',
+        'wissenschafliche Konferenz',
+
+      ),
+      new Event(
+       'Angular 2020',
+        'Lehrveranstaltung'),
+      new Event(
+       'Angular Tour',
+        'Industrietage')
+    ];
   }
 
 }
--- src/app/events/model/event.ts
+++ src/app/events/model/event.ts
@@ -1,8 +1,5 @@
 export class Event {
   constructor(
-              public eventname: string,
-              public position: string,
-              public room: number,
-              public phone: number,
-              public email: string) {}
+              public name: string,
+              public kind: string) {}
 }
