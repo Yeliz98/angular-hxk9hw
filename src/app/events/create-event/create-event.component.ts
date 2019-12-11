dce8c492f7d8c8b50fc82cb2c264d3f73d169bed
Yeliz98 <yelizoe98@hotmail.de>
2019-12-11 22:09:09 +0100

    create-events

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {Event} from '../../model/event';
+import {EventService} from '../../services/event.service';
 
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
   styleUrls: ['./create-event.component.css']
 })
-export class CreateEventComponent implements OnInit {
+export class CreateEventComponent{
 
-  constructor() { }
+  public message = '';
+  constructor(private eventService: EventService) { }
 
-  ngOnInit() {
+  createEvents(eventForm) {
+    if (eventForm.invalid) {
+      this.message = 'Please correct all errors and resubmit the form';
+    } else {
+      const event: Event = eventForm.value.event;
+      this.eventService.createEvents(event);
+    }
   }
-
 }
